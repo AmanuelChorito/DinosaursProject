53d2c72591b2554cb911eed760c802f1d8fd0e92
AmanuelChorito <48349483+AmanuelChorito@users.noreply.github.com>
2023-07-04 18:21:49 -0500

    get data from form

--- app.js
+++ app.js
@@ -1,15 +1,57 @@
 
     // Create Dino Constructor
-
+class Dino{
+    constructor(species,image,fact){
+        this.species=species;
+        this.image=image;
+        this.fact=fact;
+    }
+}
 
     // Create Dino Objects
-
+ const anklyosaurus=new Dino('anklyosaurus','anklyosaurus.png',"Night vision");
 
     // Create Human Object
+     class Human {
+    constructor(name, height, weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+
+    }
+};
+ 
+
 
     // Use IIFE to get human data from form
+   const getHumanData= (function () {
+    var HumanName='' ;
+        let height='' ;
+        let weight='';
+    let button= document.getElementById('btn');
+    button.addEventListener('click',function(){
+         HumanName =document.getElementById('name').value;
+         console.log(`internal ${HumanName}`);
+    });
+        
+        function getName(){
+            console.log(`Data: ${HumanName}`)
 
-
+            return HumanName;
+        }
+        // function getHeight(){
+        //     return height;
+        // }
+        // function getWeight(){
+        //     return weight;
+        // }
+        return{
+            name:getName,
+            // height:getHeight,
+            // weight:getWeight
+        };
+    })();
+ console.log(`Data: ${getHumanData.name()}`)
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
 
